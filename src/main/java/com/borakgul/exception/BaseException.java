76c27e18293d8c4d7aea4f74a3bd06a70d981e96
Bora Akgül <borakgul1@gmail.com>
2025-07-02 09:31:50 +0300

    Creating Exception Structure

---
+++ src/main/java/com/borakgul/exception/BaseException.java
@@ -0,0 +1,11 @@
+package com.borakgul.exception;
+
+public class BaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public BaseException(ErrorMessage errorMessage) {
+		super(errorMessage.prepareErrorMessage());
+	}
+	
+}
